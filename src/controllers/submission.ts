86418fb2e2e21b39261c199942d63565fe13439d
yash agrawal <agarwalshivam834@gmail.com>
2025-09-23 23:39:32 +0530

    add api for getting submission by challengeid

--- src/controllers/submission.ts
+++ src/controllers/submission.ts
@@ -1,7 +1,8 @@
 import { submitSubmissionRequest, submitSubmissionSchema } from "../types/submission.types";
 import { ErrorResponse, SuccessResponse } from "../types/common.types";
 import { RequestHandler } from "express-serve-static-core";
-import { createSubmission } from "../repo/submission";
+import { createSubmission, fetchSubmissionByChallengeIdAndUserId } from "../repo/submission";
+import { Request, Response } from "express";
 
 export const submitChallengeController: RequestHandler = async (req, res) => {
     try {
@@ -42,4 +43,41 @@ export const submitChallengeController: RequestHandler = async (req, res) => {
             message: 'Failed to submit challenge'
         } as ErrorResponse);
     }
+}
+
+export const getUserSubmissionByChallengeId = async (
+    req: Request<{},{},{}, {challengeId: string}> & {userId? : string},
+    res: Response<SuccessResponse | ErrorResponse>
+) => {
+ try {
+    const challengeId = req.query.challengeId;
+    if (!challengeId) {
+        return res.status(400).json({
+          success: false,
+          message: "Challenge Id is required"
+        } as ErrorResponse)
+    }
+
+    const userId = req.userId as string
+    const submission = await fetchSubmissionByChallengeIdAndUserId(challengeId, userId);
+
+    if (!submission) {
+        return res.status(400).json({
+            success: false,
+            message: "Challenge Id is required"
+          } as ErrorResponse)
+    }
+    
+    return res.status(200).json({
+       success: true,
+       message: "Submission fetched successfully",
+       data : submission,
+    } as SuccessResponse);
+ } catch (error) {
+    console.error("error fetching submission", error);
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+      } as ErrorResponse)
+ }
 }
--- src/routes/submission.ts
+++ src/routes/submission.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
-import { submitChallengeController } from "../controllers/submission";
+import {getUserSubmissionByChallengeId, submitChallengeController } from "../controllers/submission";
 import { authenticateToken } from "../middlewares/auth";
 import { upload } from "../middlewares/multer";
 
 const submissionRouter = Router();
 
 submissionRouter.use(authenticateToken)
+
 submissionRouter.post('/submit', upload.fields([
     { name: 'images', maxCount: 5 },
     { name: 'videos', maxCount: 2 }
 ]), submitChallengeController);
+
+submissionRouter.get("/", getUserSubmissionByChallengeId)
 
 export default submissionRouter;
